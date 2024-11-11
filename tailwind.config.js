fbb02acb7088ff61e24edcf5e5205eabdf962208
fardi0710 <fardi.hamdan@gmail.com>
2024-11-11 21:46:15 +0700

    add reposiory

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily:{
+      OpenSans:['Open Sans', 'sans-serif'],
+      QwitcherGrypen: ['Qwitcher Grypen', 'cursive'],
+      Kanit:['Kanit', 'sans-serif'],
+      Playfair:['Playfair', 'serif']
+    }
+  },
+  plugins: [],
+}
